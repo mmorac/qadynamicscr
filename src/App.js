cba1c8823e611621f93864babd546aca065d1825
Mario Mora <marioemorac@gmail.com>
2025-04-24 01:19:29 +0200

    Cambiando a React app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <img src="logo.png" alt="Logo" className="logo" />
+      <h1 className="title">En construcción</h1>
+      <h2 className="subtitle">
+        Puedes contactarnos al correo <span className="email">info@qadynamicscr.com</span>
+      </h2>
+    </div>
+  );
+}
+
+export default App;
